b053d2631c0372057d0cf0d098a7f2105d49073b
najwanan <theunnamedrd@gmail.com>
2022-08-15 22:03:56 -0700

    debugging db connection

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const express = require('express')
-const dotenv = require('dotenv')
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 
-//load config
+dotenv.config({ path: "./config/config.env" });
+connectDB();
 
-dotenv.config({ path: "./config/config-env" });
-
-const app = express()
-
-const PORT = process.env.PORT || 5000
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.listen(
-    PORT, 
-    console.log(`Server is running on ${process.env.NODE_ENV} on ${PORT} betta go catch it`)
-)
-
+  PORT,
+  console.log(
+    `Server is running on ${process.env.NODE_ENV} on ${PORT} betta go catch it`
+  )
+);
