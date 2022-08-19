23c042d0c1971bad8a0367cb55715d9f3fe89305
najwanan <theunnamedrd@gmail.com>
2022-08-18 19:03:15 -0700

    setting up index files

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
 const connectDB = require("./config/db");
 
 
@@ -8,6 +10,19 @@ dotenv.config({ path: "./config/config.env" })
 connectDB()
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+//Handlebars 
+app.engine('.hbs', exphbs.engine({
+  defaultLayout: 'main', 
+  extname: '.hbs'
+}
+))
+app.set('view engine', '.hbs')
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
